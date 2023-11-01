4b63d79316f34f416c38a8ee8f2c5028b0cec3cf
Sakthi Janani SR <115342115+Sakthi29032001@users.noreply.github.com>
2023-11-01 18:37:42 +0530

    Add files via upload

---
+++ Task 1/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle 
+import pandas as pd
+
+#reading the encoder, model and scaler object files
+encoder = pickle.load(open("encoder.pkl", 'rb'))
+model = pickle.load(open("model.pkl", 'rb'))
+scaler = pickle.load(open("scaler.pkl", 'rb'))
+
+#setting the title and text
+st.title("TASK 1 : Iris Flower Classification")
+st.write("**NAME : Sakthi Janani SR**")
+
+#taking the input from user
+newSL = st.number_input("Enter SepalLength (cm):", min_value=0.0)
+newSW = st.number_input("Enter SepalWidth (cm):", min_value=0.0)
+newPL = st.number_input("Enter PetalLength (cm):", min_value=0.0)
+newPW = st.number_input("Enter PetalWidth (cm):", min_value=0.0)
+
+#button to trigger the classification
+if st.button("Classify"):
+    newValue = pd.DataFrame([[newSL, newSW, newPL, newPW]])
+    newValue = scaler.transform(newValue)
+    prediction = model.predict(newValue)
+    finalAns = encoder.inverse_transform(prediction)
+    st.markdown(f"Prediction result: **{finalAns[0]}**")
+
+
